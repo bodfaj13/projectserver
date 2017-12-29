b40a34a76b6bfa9a09cf9e5749da5360bf646af7
Bello Ajibola <bellohargbola13@gmail.com>
2017-12-29 07:51:34 +0100

    tested tru

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+if(!process.env.PORT){
+  console.log('server started at '+ port);
+}
